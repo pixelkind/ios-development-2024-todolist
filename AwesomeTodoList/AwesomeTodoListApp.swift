c5909f5a63dbd63acd09c12405a85f52d753105a
Garrit <garrit.schaap@ju.se>
2024-01-16 13:27:05 +0100

    Initial Commit

---
+++ AwesomeTodoList/AwesomeTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeTodoListApp.swift
+//  AwesomeTodoList
+//
+//  Created by Garrit Schaap on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AwesomeTodoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AwesomeTodoList
+//
+//  Created by Garrit Schaap on 2024-01-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
